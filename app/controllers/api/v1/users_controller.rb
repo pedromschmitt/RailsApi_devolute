96cd0ed68edf7c52de3f513bd26b411b57107c17
Pedro Schmitt <pedro.schmitt@waveweb2.com>
2020-04-10 12:47:37 +0100

    Generate User controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  def current
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post 'user_token' => 'user_token#create'
+      get 'users/create'
+      get 'users/destroy'
+      get 'users/update'
+      get 'users/current'
+      get 'users/show'
     end
   end
 end
