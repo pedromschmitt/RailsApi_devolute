7d85e78d988e2b4f96929d4ac7e7b443e54427d7
Pedro Schmitt <pedro.schmitt@waveweb2.com>
2020-04-10 17:35:23 +0100

    Create validation tests for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Validation tests
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_length_of(:password).is_at_least(8).is_at_most(72) }
 end
