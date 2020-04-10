00a5275cd89caf39db38fc03ff36d40ae6c240f5
Pedro Schmitt <pedro.schmitt@waveweb2.com>
2020-04-10 17:36:52 +0100

    Set User model validations. Tests passing now.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
+  validates_length_of :password,
+                      maximum: 72,
+                      minimum: 8,
+                      allow_nil: true,
+                      allow_blank: false
+ 
+  validates_presence_of :name, :email
 end
