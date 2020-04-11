f949b860f53c832a1f1d954c30b8cc48a227e319
Pedro Schmitt <pedro.schmitt@waveweb2.com>
2020-04-11 21:36:23 +0100

    Remove automatic spec created by system.

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -1,40 +1,80 @@
 require 'rails_helper'
+require 'rspec/json_expectations'
 
 RSpec.describe "Api::V1::Users", type: :request do
+  describe "GET /api/v1/users/:id" do
+    context 'when user exists' do
+      let(:user) {create(:user)}
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/api/v1/users/create"
-      expect(response).to have_http_status(:success)
+      before do
+        get "/api/v1/users/#{user.id}"
+      end
+
+      it { expect(response).to have_http_status(:success)}
+
+      it 'returns valid user in json' do
+        expect(json).to eql.(serialized(Api::V1::UserSerializer, user))
+      end
+    end
+
+    context 'when user dont exist' do
+      let(:user_id) { -1 }
+      
+      before { get "/api/v1/users/#{user.id}" }
+
+      it { expect(response).to have_http_status(:not_found)}
     end
   end
 
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/api/v1/users/destroy"
-      expect(response).to have_http_status(:success)
+  describe 'GET /api/v1/users/current' do
+    context 'Unauthenticated' do
+      it_behaves_like :deny_without_authorization, :get, '/api/v1/users/current'
     end
+
+    context 'Authenticated' do
+      let(:user) { create(:user) }
+
+      before do
+        get '/api/v1/users/current', headers: header_with_authentication(user)
+      end
+
+      it { expect(response).to have_http_status(:success) }
+
+      it 'returns valid user in json' do
+        expect(json).to eql(serialized(Api::V1::UserSerializer, user))
+      end
+    end
+
   end
 
-  describe "GET /update" do
-    it "returns http success" do
-      get "/api/v1/users/update"
-      expect(response).to have_http_status(:success)
+  describe 'POST  /api/v1/users' do
+    context 'Valid params' do
+      let(:user_params) { attributes_for(:user) }
+
+      it 'return created' do
+        post '/api/v1/users/', params: { user: user_params }
+        expect(response).to have_http_status(:created)
+      end
+
+      it 'returns right user in json' do
+        post '/api/v1/users/', params: { user: user_params }
+        expect(json).to include_json(user_params.except(:password))
+      end
+
+      it 'create user' do
+        expect do
+          post '/api/v1/users/', params: { user: user_params }
+        end.to change { User.count }.by(1)
+      end
+    end
+
+    context 'Invalid params' do
+      let(:user_params) { {foo: :bar} }
+
+      before { post '/api/v1/users/', params: { user: user_params } }
+
+      it { expect(response).to have_http_status(:unprocessable_entity) }
     end
   end
-
-  describe "GET /current" do
-    it "returns http success" do
-      get "/api/v1/users/current"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/api/v1/users/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
+  
 end
