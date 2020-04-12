e81a11703985c6badd62c1f1e1ac583f8c0db408
Pedro Schmitt <pedro.schmitt@waveweb2.com>
2020-04-12 20:03:50 +0100

    Add Image model test: validate_presence_of(:image)

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Image, type: :model do
   it { should belong_to(:user) }
   it { should validate_presence_of(:user_id) }
+  it { should validate_presence_of(:image) }
 
 end
