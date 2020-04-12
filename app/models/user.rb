4fd9b9a7220f24edf35c1a4b1989a33013e397ba
Pedro Schmitt <pedro.schmitt@waveweb2.com>
2020-04-12 18:07:39 +0100

    add images association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_one_attached :photo
-  has_many_attached :images
+  has_many :images
   has_secure_password
   validates_length_of :password,
                       maximum: 72,
