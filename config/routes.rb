bcdce7a4714e995a19650ee0b7e29c67e9794738
Pedro Schmitt <pedro.schmitt@waveweb2.com>
2020-04-12 08:27:46 +0100

    Add 'get current' route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post 'user_token' => 'user_token#create'
-      resources :users
+      resources :users, only: %i[show create update destroy] do
+        # 'on: :collectin' remove :id from path
+        get 'current', on: :collection
+      end
     end
   end
 end
