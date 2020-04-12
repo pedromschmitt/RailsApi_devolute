0959525185cb1ab7656089cb49a34270de8edfae
Pedro Schmitt <pedro.schmitt@waveweb2.com>
2020-04-12 20:38:04 +0100

    add images count to user serializer

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,8 +1,11 @@
 module Api
   module V1
     class UserSerializer < ActiveModel::Serializer
-      attributes :id, :name, :email, :photo
+      attributes :id, :name, :email, :photo, :images_count
 
+      def images_count
+        object.images.count
+      end
     end
   end
 end
