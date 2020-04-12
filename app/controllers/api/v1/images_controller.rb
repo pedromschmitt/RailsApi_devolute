cba1c29181763e747ee9f5efea8e8f52e23f8524
Pedro Schmitt <pedro.schmitt@waveweb2.com>
2020-04-12 20:06:56 +0100

    Set create

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -11,6 +11,19 @@ class Api::V1::ImagesController < ApplicationController
   end
 
   def create
+    @image = Image.new(image_params.merge(user: current_user))
+ 
+    if @image.save
+      render json: @image, status: :created
+    else
+      render json: @image.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:title, :image)
   end
 
 end
