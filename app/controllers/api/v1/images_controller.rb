28cbf6a5e05e8a2387db0765ee423db903f55a15
Pedro Schmitt <pedro.schmitt@waveweb2.com>
2020-04-12 20:11:01 +0100

    Change namespace, to Images Controller recognize " before_action :authenticate_user" method

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -1,29 +1,33 @@
-class Api::V1::ImagesController < ApplicationController
-  before_action :authenticate_user
+module Api
+  module V1
+    class ImagesController < Api::V1::ApiController
+      before_action :authenticate_user
 
-  def index
-    user = User.find params[:user_id]
-    @images = user.images
-    render json: @images
-  end
+      def index
+        user = User.find params[:user_id]
+        @images = user.images
+        render json: @images
+      end
 
-  def show
-  end
+      def show
+      end
 
-  def create
-    @image = Image.new(image_params.merge(user: current_user))
- 
-    if @image.save
-      render json: @image, status: :created
-    else
-      render json: @image.errors, status: :unprocessable_entity
+      def create
+        @image = Image.new(image_params.merge(user: current_user))
+    
+        if @image.save
+          render json: @image, status: :created
+        else
+          render json: @image.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def image_params
+        params.require(:image).permit(:title, :image)
+      end
+
     end
   end
-
-  private
-
-  def image_params
-    params.require(:image).permit(:title, :image)
-  end
-
 end
