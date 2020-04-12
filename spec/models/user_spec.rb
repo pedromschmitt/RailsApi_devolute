fd078f380eb82742db0ccb09e895bb9f915d793d
Pedro Schmitt <pedro.schmitt@waveweb2.com>
2020-04-12 18:39:21 +0100

    Create test for destroy dependency user/images

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,5 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:email) }
   it { should validate_length_of(:password).is_at_least(8).is_at_most(72) }
+  it { should have_many(:images).dependent(:destroy) }
 end
