75bf037470b79d07ec3fba7bc84ea80fd4ecf4c4
Pedro Schmitt <pedro.schmitt@waveweb2.com>
2020-04-12 08:44:02 +0100

    Rescue errors and render as json for client response

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -2,6 +2,17 @@ module Api
   module V1
     class ApiController < ApplicationController
       include Knock::Authenticable
+      serialization_scope :current_user
+
+      rescue_from ActiveRecord::RecordNotFound do |msg|
+        render(json: { message: msg }, status: :not_found)
+      end
+
+      rescue_from ActionController::ParameterMissing do |exception|
+        render(json: { message: exception.param }, status: :bad_request)
+      end
+
+      
     end
   end
 end
