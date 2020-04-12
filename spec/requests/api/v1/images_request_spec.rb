ca78c41da15c066bb5d247ed5d258cbc7d06ac14
Pedro Schmitt <pedro.schmitt@waveweb2.com>
2020-04-12 19:35:04 +0100

    Create tests for images

--- spec/requests/api/v1/images_request_spec.rb
+++ spec/requests/api/v1/images_request_spec.rb
@@ -1,33 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Images", type: :request do
+  describe 'GET /api/v1/images?user_id=:id' do
+    context 'User exists' do
+      let(:user) { create(:user) }
+      let(:images_number) { Random.rand(1..5) }
 
-  describe "GET /index" do
-    it "returns http success" do
-      get "/api/v1/images/index"
-      expect(response).to have_http_status(:success)
-    end
+      before { images_number.times { create(:image, user: user) } }
+
+      it do
+        get "/api/v1/images?user_id=#{user.id}", headers: header_with_authentication(user)
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'returns right images' do
+        get "/api/v1/images?user_id=#{user.id}", headers: header_with_authentication(user)
+        expect(json).to eql(each_serialized(Api::V1::ImageSerializer, user.images))
+      end
+    
   end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/api/v1/images/show"
-      expect(response).to have_http_status(:success)
+    context 'User dont exist' do
+      let(:user) { create(:user) }
+      let(:user_id) { -1 }
+
+      before { get "/api/v1/images?user_id=#{user_id}", headers: header_with_authentication(user) }
+
+      it { expect(response).to have_http_status(:not_found) }
     end
   end
-
-  describe "GET /create" do
-    it "returns http success" do
-      get "/api/v1/images/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/api/v1/images/destroy"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
