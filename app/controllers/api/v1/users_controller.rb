86bf40c15bfff77c155d2afb75357385c519286e
Pedro Schmitt <pedro.schmitt@waveweb2.com>
2020-04-12 17:11:38 +0100

    Install active storage and set photo and images to user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -27,7 +27,7 @@ module Api
       private 
 
       def user_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        params.require(:user).permit(:name, :email, :password, :password_confirmation, :photo, images: [])
       end
 
       def set_user
